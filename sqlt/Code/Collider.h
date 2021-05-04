c22ca3b6055a4a296438cbec6c0445eb16d59d06
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-05-04 14:41:23 +0200

    collisions init

--- sqlt/Code/Collider.h
+++ sqlt/Code/Collider.h
@@ -1,54 +1,36 @@
 #ifndef __COLLIDER_H__
 #define __COLLIDER_H__
 
-#include "SDL/include/SDL_Rect.h"
+#include "SDL_Rect.h"
 
 #define MAX_LISTENERS 300
 
-class Module;
+enum class ColliderType
+{
+	PLAYER,
+	WALL,
+
+
+	MAX
+};
 
 struct Collider
 {
-	enum Type
-	{
-		NONE = -1,
-		PLAYER,
-		FRONTPLAYER,
-		WALL,
-		TP1TO2,
-		TP2TO1,
-		NPC,
-		ENEMY,
-		PENCIL,
+	SDL_Rect rect;
+	bool pendingToDelete = false;
+	ColliderType type;
 
-		MAX
-	};
-
-	enum Height
-	{
-		UNDEFINED
-	};
-
-	//Methods
-	Collider(SDL_Rect rectangle, Type type, Module* listener = nullptr);
+	Collider(SDL_Rect rectangle, ColliderType type);
 
 	void SetPos(int x, int y);
 
 	bool Intersects(const SDL_Rect& r) const;
-
-	void AddListener(Module* listener);
-
-	//Variables
-	SDL_Rect rect;
-	bool pendingToDelete = false;
-	Type type;
-	Height height;
-	Module* listeners[MAX_LISTENERS] = { nullptr };
 };
 
-Collider::Collider(SDL_Rect rectangle, Type type, Module* listener) : rect(rectangle), type(type)
+Collider::Collider(SDL_Rect rectangle, ColliderType _type)
 {
-	listeners[0] = listener;
+	rect = rectangle;
+	type = _type;
 }
 
 void Collider::SetPos(int x, int y)
@@ -63,22 +45,7 @@ bool Collider::Intersects(const SDL_Rect& r) const
 		rect.x + rect.w > r.x &&
 		rect.y < r.y + r.h &&
 		rect.h + rect.y > r.y);
-}
-
-void Collider::AddListener(Module* listener)
-{
-	for (int i = 0; i < MAX_LISTENERS; ++i)
-	{
-		if (listeners[i] == nullptr)
-		{
-			listeners[i] = listener;
-			break;
-		}
-
-		//Simple security check to avoid adding the same listener twice
-		else if (listeners[i] == listener)
-			break;
-	}
+	return SDL_HasIntersection(&rect, &r);
 }
 
 #endif
--- sqlt/Code/Collisions.h
+++ sqlt/Code/Collisions.h
@@ -3,124 +3,59 @@
 
 #define MAX_COLLIDERS 2000
 
-#include "Module.h"
 #include "Collider.h"
+#include "Share.h"
 
-class Collisions : public Module
+class Collisions
 {
 public:
-	Collisions(bool startEnabled);
-
-	// Destructor
+	Collisions();
 	virtual ~Collisions() {}
 
-	// Called before render is available
-	bool Awake(pugi::xml_node&);
+	void Update(float dt);
+	void Draw();
+	void CleanUp();
 
-	// Called before the first frame
-	bool Start();
+	Collider* AddCollider(SDL_Rect rect, ColliderType type);
+	Collider* AddDynamicCollider(SDL_Rect rect, ColliderType type);
 
-	// Called each loop iteration
-	bool PreUpdate();
-	bool Update(float dt);
-	bool PostUpdate();
-
-	// Called before quitting
-	bool CleanUp();
-
-	// Adds a new collider to the list
-	Collider* AddCollider(SDL_Rect rect, Collider::Type type, Module* listener = nullptr);
-	Collider* AddDynamicCollider(SDL_Rect rect, Collider::Type type, Module* listener = nullptr);
-
-	// Removes the collider memory and removes it from the colliders array
 	void RemoveCollider(Collider* collider);
 
-	// Draws all existing colliders with some transparency
-	void DebugDraw();
 	void DebugRequest();
 
-
-	bool matrix[Collider::Type::MAX][Collider::Type::MAX];
+	bool matrix[ColliderType::MAX][ColliderType::MAX];
 
 private:
-	// All existing colliders in the scene
 	Collider* colliders[MAX_COLLIDERS] = { nullptr };
 
 	// The collision matrix. Defines the interaction for two collider types
 	// If set two false, collider 1 will ignore collider 2
 
-
-	// Simple debugging flag to draw all colliders
 	bool debug = false;
 };
 
-#include "Collisions.h"
-
-#include "App.h"
-#include "Input.h"
-#include "Log.h"
-#include "Render.h"
-
-Collisions::Collisions(bool startEnabled) : Module()
+Collisions::Collisions()
 {
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
-		colliders[i] = nullptr;
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i) colliders[i] = nullptr;
 
-	matrix[Collider::Type::PLAYER][Collider::Type::WALL] = true;
-	matrix[Collider::Type::PLAYER][Collider::Type::PLAYER] = false;
-	matrix[Collider::Type::PLAYER][Collider::Type::NPC] = true;
-	matrix[Collider::Type::PLAYER][Collider::Type::ENEMY] = true;
-	matrix[Collider::Type::PLAYER][Collider::Type::TP1TO2] = true;
-	matrix[Collider::Type::PLAYER][Collider::Type::TP2TO1] = true;
+	matrix[ColliderType::PLAYER][ColliderType::PLAYER] = false;
+	matrix[ColliderType::PLAYER][ColliderType::WALL] = true;
+	matrix[ColliderType::PLAYER][ColliderType::MAX] = false;
 
-	matrix[Collider::Type::WALL][Collider::Type::WALL] = false;
-	matrix[Collider::Type::WALL][Collider::Type::PLAYER] = true;
+	matrix[ColliderType::WALL][ColliderType::PLAYER] = true;
+	matrix[ColliderType::WALL][ColliderType::WALL] = false;
+	matrix[ColliderType::WALL][ColliderType::MAX] = false;
 
-	matrix[Collider::Type::NPC][Collider::Type::PLAYER] = true;
-
-	matrix[Collider::Type::ENEMY][Collider::Type::PLAYER] = true;
-
-	matrix[Collider::Type::FRONTPLAYER][Collider::Type::PENCIL] = true;
-	matrix[Collider::Type::FRONTPLAYER][Collider::Type::FRONTPLAYER] = false;
-
-	matrix[Collider::Type::PENCIL][Collider::Type::FRONTPLAYER] = true;
-	matrix[Collider::Type::PENCIL][Collider::Type::PENCIL] = false;
-
-	matrix[Collider::Type::TP1TO2][Collider::Type::PLAYER] = true;
-	matrix[Collider::Type::TP1TO2][Collider::Type::TP1TO2] = false;
-
-	matrix[Collider::Type::TP2TO1][Collider::Type::PLAYER] = true;
-	matrix[Collider::Type::TP2TO1][Collider::Type::TP2TO1] = false;
-
-
+	matrix[ColliderType::MAX][ColliderType::PLAYER] = false;
+	matrix[ColliderType::MAX][ColliderType::WALL] = false;
+	matrix[ColliderType::MAX][ColliderType::MAX] = false;
 }
 
-// Called before render is available
-bool Collisions::Awake(pugi::xml_node&)
+void Collisions::Update(float dt)
 {
-	return true;
-}
-
-// Called before the first frame
-bool Collisions::Start()
-{
-	return true;
-}
-
-// Called each loop iteration
-bool Collisions::PreUpdate()
-{
-
-
-	return true;
-}
-
-bool Collisions::Update(float dt)
-{
-	// Remove all colliders scheduled for deletion
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
 	{
-		if (colliders[i] != nullptr && colliders[i]->pendingToDelete == true)
+		if (colliders[i] && colliders[i]->pendingToDelete)
 		{
 			delete colliders[i];
 			colliders[i] = nullptr;
@@ -130,73 +65,77 @@ bool Collisions::Update(float dt)
 	Collider* c1;
 	Collider* c2;
 
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
 	{
 		// skip empty colliders
-		if (colliders[i] == nullptr)
-			continue;
+		if (!colliders[i]) continue;
 
 		c1 = colliders[i];
 
 		// avoid checking collisions already checked
-		for (uint k = i + 1; k < MAX_COLLIDERS; ++k)
+		for (unsigned int k = i + 1; k < MAX_COLLIDERS; ++k)
 		{
 			// skip empty colliders
-			if (colliders[k] == nullptr)
-				continue;
+			if (!colliders[k]) continue;
 
 			c2 = colliders[k];
 
 			if (matrix[c1->type][c2->type] && c1->Intersects(c2->rect))
 			{
-				for (uint i = 0; i < MAX_LISTENERS; ++i)
-					if (c1->listeners[i] != nullptr) c1->listeners[i]->OnCollision(c1, c2);
+				for (unsigned int i = 0; i < MAX_LISTENERS; ++i)
+					if (c1->listeners[i]) c1->listeners[i]->OnCollision(c1, c2);
 
-				for (uint i = 0; i < MAX_LISTENERS; ++i)
-					if (c2->listeners[i] != nullptr) c2->listeners[i]->OnCollision(c2, c1);
+				for (unsigned int i = 0; i < MAX_LISTENERS; ++i)
+					if (c2->listeners[i]) c2->listeners[i]->OnCollision(c2, c1);
 			}
 
 		}
 	}
-
-	return true;
 }
 
-bool Collisions::PostUpdate()
+void Collisions::Draw()
 {
 	if (debug)
-		DebugDraw();
+	{
+		Uint8 alpha = 128;
+		for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
+		{
+			if (!colliders[i]) continue;
 
+			switch (colliders[i]->type)
+			{
+			case ColliderType::PLAYER: SetRenderDrawColor(0, 255, 0, alpha); break;
+			case ColliderType::WALL: SetRenderDrawColor(255, 0, 0, alpha); break;
+			case ColliderType::MAX: SetRenderDrawColor(0, 0, 0, alpha); break;
+			}
 
-	return true;
+			RenderFillRect(colliders[i]->rect.x, colliders[i]->rect.y, colliders[i]->rect.w, colliders[i]->rect.h);
+		}
+	}
 }
 
-// Called before quitting
-bool Collisions::CleanUp()
+void Collisions::CleanUp()
 {
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
 	{
-		if (colliders[i] != nullptr)
+		if (colliders[i])
 		{
 			delete colliders[i];
 			colliders[i] = nullptr;
 		}
 	}
-
-	return true;
 }
 
-Collider* Collisions::AddCollider(SDL_Rect rect, Collider::Type type, Module* listener)
+Collider* Collisions::AddCollider(SDL_Rect rect, ColliderType type)
 {
 	Collider* ret = nullptr;
 
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
 	{
-		if (colliders[i] == nullptr)
+		if (!colliders[i])
 		{
-			ret = colliders[i] = new Collider(rect, type, listener);
+			ret = colliders[i] = new Collider(rect, type);
 			break;
-			LOG("%d ", i);
 		}
 	}
 
@@ -205,47 +144,12 @@ Collider* Collisions::AddCollider(SDL_Rect rect, Collider::Type type, Module* li
 
 void Collisions::RemoveCollider(Collider* collider)
 {
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
+	for (unsigned int i = 0; i < MAX_COLLIDERS; ++i)
 	{
 		if (colliders[i] == collider)
 		{
 			delete colliders[i];
 			colliders[i] = nullptr;
-		}
-	}
-}
-
-void Collisions::DebugDraw()
-{
-	Uint8 alpha = 80;
-	for (uint i = 0; i < MAX_COLLIDERS; ++i)
-	{
-		if (colliders[i] == nullptr)
-			continue;
-
-		switch (colliders[i]->type)
-		{
-		case Collider::Type::NONE: // white
-			app->render->DrawRectangle(colliders[i]->rect, 255, 255, 255, alpha);
-			break;
-
-		case Collider::Type::WALL:
-			app->render->DrawRectangle(colliders[i]->rect, 0, 255, 0, alpha);
-
-		case Collider::Type::PLAYER:
-			app->render->DrawRectangle(colliders[i]->rect, 0, 0, 255, alpha);
-			break;
-		case Collider::Type::NPC:
-			app->render->DrawRectangle(colliders[i]->rect, 255, 0, 255, alpha);
-			break;
-		case Collider::Type::PENCIL:
-			app->render->DrawRectangle(colliders[i]->rect, 255, 0, 150, alpha);
-			break;
-		case Collider::Type::FRONTPLAYER:
-			app->render->DrawRectangle(colliders[i]->rect, 50, 0, 90, alpha);
-			break;
-		case Collider::Type::TP1TO2:
-			app->render->DrawRectangle(colliders[i]->rect, 50, 0, 90, alpha);
 			break;
 		}
 	}
